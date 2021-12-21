5743a1529748604a8a9a16e4177e953174ffb6cb
Jimin Kim <jackk0616@gmail.com>
2021-12-21 17:49:52 +0900

    새로운 할인 정책 개발

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,37 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 10프로 할인")
+    public void vip_o() throws Exception {
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("일반 유저는 할인 X")
+    public void vip_x() throws Exception {
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+
+}
