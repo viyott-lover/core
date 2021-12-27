5fa099fc4cf6b1b472ddf823e40d0d371294402c
Jimin Kim <jackk0616@gmail.com>
2021-12-27 17:37:24 +0900

    request 스코프 예제 + provider

---
+++ src/main/java/hello/core/web/logDemoController.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class logDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        MyLogger logger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+        logger.setRequestURL(requestURL);
+
+        logger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,35 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("destroy");
+        }
+    }
+}
